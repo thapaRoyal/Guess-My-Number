cb39530f57388b825157b5d37394353fce245fc1
Royal <thaparoyal17@gmail.com>
2021-03-08 22:10:05 +0545

    Handling Click Events

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉Correct Number';
 
@@ -8,3 +8,13 @@ document.querySelector('.score').textContent = 13;
 
 document.querySelector('.guess').value = 20;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  }
+});
