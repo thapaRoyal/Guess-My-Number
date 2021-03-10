4ca8574f94eea123f68c9dcad248b0fbbdbd3b33
Royal <thaparoyal17@gmail.com>
2021-03-11 04:02:56 +0545

    manipulating styles

--- script.js
+++ script.js
@@ -22,10 +22,16 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   // Implementing Game Logic
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number!';
+// Manipulating style
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -35,6 +41,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game !';
       document.querySelector('.score').textContent = 0;
     }
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
